0db2159a5081ed1da970688557241d3b8e917bbe
Sirius <sirius_mchao@163.com>
2020-11-02 17:30:22 +0800

    引入CustomControls

--- MWebBrowser/App.xaml.cs
+++ MWebBrowser/App.xaml.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CefSharp;
+using CefSharp.Wpf;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Windows;
 
 namespace MWebBrowser
@@ -13,5 +13,53 @@ namespace MWebBrowser
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            //Add Custom assembly resolver
+            AppDomain.CurrentDomain.AssemblyResolve += Resolver;
+
+            //Any CefSharp references have to be in another method with NonInlining
+            // attribute so the assembly rolver has time to do it's thing.
+            InitializeCefSharp();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void InitializeCefSharp()
+        { 
+            // enable High-DPI support on Windows 7 or newer.
+            Cef.EnableHighDPISupport();
+
+            var settings = new CefSettings();
+            // Set BrowserSubProcessPath based on app bitness at runtime
+            settings.BrowserSubprocessPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
+                                                   Environment.Is64BitProcess ? "x64" : "x86",
+                                                   "CefSharp.BrowserSubprocess.exe");
+
+
+           //禁用GPU
+            settings.CefCommandLineArgs.Add("disable-gpu","1");
+            settings.CefCommandLineArgs.Add("no-proxy-server","1");
+
+            // Make sure you set performDependencyCheck false
+            Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
+        }
+
+        // Will attempt to load missing assembly from either x86 or x64 subdir
+        // Required by CefSharp to load the unmanaged dependencies when running using AnyCPU
+        private static Assembly Resolver(object sender, ResolveEventArgs args)
+        {
+            if (args.Name.StartsWith("CefSharp"))
+            {
+                string assemblyName = args.Name.Split(new[] { ',' }, 2)[0] + ".dll";
+                string archSpecificPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
+                                                       Environment.Is64BitProcess ? "x64" : "x86",
+                                                       assemblyName);
+
+                return File.Exists(archSpecificPath)
+                           ? Assembly.LoadFile(archSpecificPath)
+                           : null;
+            }
+            return null;
+        }
     }
 }
---
+++ MWebBrowser/MWebBrowserUc.xaml.cs
@@ -0,0 +1,43 @@
+﻿using CefSharp.Wpf;
+using System.Windows.Controls;
+
+namespace MWebBrowser
+{
+    /// <summary>
+    /// MWebBrowserUc.xaml 的交互逻辑
+    /// </summary>
+    public partial class MWebBrowserUc : UserControl
+    {
+        ChromiumWebBrowser CefWebBrowser;
+        public MWebBrowserUc()
+        {
+            InitializeComponent();
+            CefWebBrowser = new ChromiumWebBrowser();
+            //CefWebBrowser.IsBrowserInitializedChanged += MWebBrowser_IsBrowserInitializedChanged;
+        }
+
+        public void Load(string url)
+        {
+            //CefWebBrowser.Load(url);
+        }
+
+
+        //private void MWebBrowser_IsBrowserInitializedChanged(object sender, DependencyPropertyChangedEventArgs e)
+        //{
+        //    try
+        //    {
+        //        //防止多次SetPreference，在Browser初始化完成后处理一次
+        //        if (!CefWebBrowser.IsBrowserInitialized) return;          
+        //        Cef.UIThreadTaskFactory.StartNew(() =>
+        //        {
+        //            var requestContext = CefWebBrowser.GetBrowser().GetHost().RequestContext;
+        //            requestContext.SetPreference("profile.default_content_setting_values.plugins", 1, out _);//修改插件默认参数，使flash自动加载
+        //        });
+        //    }
+        //    catch (Exception ex)
+        //    {
+
+        //    }
+        //}
+    }
+}

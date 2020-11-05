fada5b5adababf51e684c07fdbb57ed734ecf7c7
Sirius <sirius_mchao@163.com>
2020-11-05 21:12:19 +0800

    增加DownloadTool控件

--- MWebBrowser/Code/CustomCef/CustomDownloadHandler.cs
+++ MWebBrowser/Code/CustomCef/CustomDownloadHandler.cs
@@ -1,19 +1,30 @@
 ﻿using CefSharp;
+using MWebBrowser.Code.Configure;
 
 namespace MWebBrowser.Code.CustomCef
 {
-    public class CustomDownloadHandler: IDownloadHandler
+    public class CustomDownloadHandler : IDownloadHandler
     {
         public void OnBeforeDownload(IWebBrowser chromiumWebBrowser, IBrowser browser, DownloadItem downloadItem,
             IBeforeDownloadCallback callback)
         {
-
+            if (callback.IsDisposed) return;
+            downloadItem.IsInProgress = true;
+            var path = GetDownloadFullPath(downloadItem.SuggestedFileName);
+            callback.Continue(path, false);
         }
 
         public void OnDownloadUpdated(IWebBrowser chromiumWebBrowser, IBrowser browser, DownloadItem downloadItem,
             IDownloadItemCallback callback)
         {
-           
+
+        }
+
+
+        private string GetDownloadFullPath(string suggestedFileName)
+        {
+            var configPath = ConfigHelper.Config.DownLoadPath.TrimEnd('\\') + "\\";
+            return configPath + suggestedFileName;
         }
     }
 }

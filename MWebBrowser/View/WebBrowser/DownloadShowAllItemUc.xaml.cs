f82e9949e0df6ea823a27258b57c43e03e8f27d0
Sirius <sirius_mchao@163.com>
2020-12-04 09:53:22 +0800

    添加DownloadShowAll用户控件

---
+++ MWebBrowser/View/WebBrowser/DownloadShowAllItemUc.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace MWebBrowser.View.WebBrowser
+{
+    /// <summary>
+    /// Interaction logic for DownloadShowAllItemUc.xaml
+    /// </summary>
+    public partial class DownloadShowAllItemUc : UserControl
+    {
+        public DownloadShowAllItemUc()
+        {
+            InitializeComponent();
+        }
+    }
+}

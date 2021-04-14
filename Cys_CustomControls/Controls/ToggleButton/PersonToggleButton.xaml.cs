cd824e2a519e1f34eda73dc8763e8bbde1c164b1
Sail <sirius_mchao@163.com>
2021-04-14 11:23:57 +0800

    增加个人信息按钮

---
+++ Cys_CustomControls/Controls/ToggleButton/PersonToggleButton.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace Cys_CustomControls.Controls
+{
+    /// <summary>
+    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:Cys_CustomControls.Controls.ToggleButton"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:Cys_CustomControls.Controls.ToggleButton;assembly=Cys_CustomControls.Controls.ToggleButton"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives
+    /// to this project and Rebuild to avoid compilation errors:
+    ///
+    ///     Right click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Browse to and select this project]
+    ///
+    ///
+    /// Step 2)
+    /// Go ahead and use your control in the XAML file.
+    ///
+    ///     <MyNamespace:PersonToggleButton/>
+    ///
+    /// </summary>
+    public class PersonToggleButton : ToggleButton
+    {
+        static PersonToggleButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PersonToggleButton), new FrameworkPropertyMetadata(typeof(PersonToggleButton)));
+        }
+
+        #region ==ButtonImage 按钮图片==
+        /// <summary>
+        /// ButtonImage
+        /// </summary>
+        public static readonly DependencyProperty ButtonImageProperty =
+            DependencyProperty.Register("ButtonImage", typeof(ImageSource), typeof(PersonToggleButton));
+        public ImageSource ButtonImage
+        {
+            get => (ImageSource)GetValue(ButtonImageProperty); 
+            set => SetValue(ButtonImageProperty, value);
+        }
+        #endregion ==ButtonImage==
+    }
+}
---
+++ MWebBrowser/View/Setting/Person/PersonMenuUc.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Controls;
+
+namespace MWebBrowser.View.Setting.Person
+{
+    /// <summary>
+    /// Interaction logic for PersonUc.xaml
+    /// </summary>
+    public partial class PersonMenuUc : UserControl
+    {
+        public PersonMenuUc()
+        {
+            InitializeComponent();
+        }
+
+        private void PersonButton_OnChecked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            PersonPop.IsOpen = true;
+        }
+
+        private void PersonButton_OnUnchecked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            PersonPop.IsOpen = false;
+        }
+
+        private void ManagePerson_OnClick(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+    }
+}
